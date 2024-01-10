ffa2d2364c1e04df0efa8deb42f48cca1564062d
Aymen EZZAYER <aymen.ezzayer@epita.fr>
2024-01-10 01:43:43 +0100

    ✨ Add `/randomtopics` command !

--- bot.py
+++ bot.py
@@ -1,41 +1,57 @@
 from dotenv import load_dotenv
-from telegram import Update
-from telegram.ext import Application, CommandHandler, CommandHandler, CallbackContext
+from telethon.sync import TelegramClient, events
+from telethon.tl.custom.message import Message
 
-import logging
+from src.forum.random_topics import get_all_topics, random_topics
+
 import os
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Retrieve the token from the environment variables
-TOKEN = os.getenv("API_TOKEN")
-if not TOKEN:
+# Retrieve the environment variables
+API_TOKEN = os.getenv("API_TOKEN")
+if not API_TOKEN:
     raise ValueError("API_TOKEN not found in .env file")
 
-# Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-logger = logging.getLogger(__name__)
+API_ID = os.getenv("API_ID")
+if not API_ID:
+    raise ValueError("API_ID not found in .env file")
 
-# Log the TOKEN
-logger.info(f"TOKEN: {TOKEN}")
+API_HASH = os.getenv("API_HASH")
+if not API_HASH:
+    raise ValueError("API_HASH not found in .env file")
 
-async def hello(update: Update, context: CallbackContext) -> None:
-    """Send a salutation when the user sends /hello."""
-    text = '👋 Greetings !\n\nMy name is Aléato !'
+USER_SESSION = os.getenv("USER_SESSION")
+if not USER_SESSION:
+    raise ValueError("USER_SESSION not found in .env file")
 
-    await update.message.reply_text(text)
+BOT_SESSION = os.getenv("BOT_SESSION")
+if not BOT_SESSION:
+    raise ValueError("BOT_SESSION not found in .env file")
 
-def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    application = Application.builder().token(TOKEN).build()
+bot_client = TelegramClient(BOT_SESSION, API_ID, API_HASH).start(bot_token=API_TOKEN)
 
-    # Add the /hello command handler
-    application.add_handler(CommandHandler("hello", hello))
+@bot_client.on(events.NewMessage(pattern='/randomtopics'))  # Command to trigger the bot
+async def random_topics_command(event: Message) -> None:
+    try:
+        # Get the ID of the chat that called the bot
+        chat_id = event.chat_id
+        peer_id = event.message.peer_id.channel_id 
+        
+        topics = await get_all_topics(chat_id, USER_SESSION, API_ID, API_HASH)
 
-    # Run the bot until the user presses Ctrl-C
-    application.run_polling(allowed_updates=Update.ALL_TYPES)
+        random_count = 5
+        randoms = random_topics(random_count, topics)
 
-if __name__ == "__main__":
-    main()
+        # Send a message with the link to the forum topic
+        response_string = ""
+        for i, topic in enumerate(randoms, start = 1):
+            response_string += f"{i}. {topic.title}\n    -> t.me/c/{peer_id}/{topic.id}\n"
+        
+        await event.reply(response_string)
+    except Exception as e:
+        print(f"[COMMAND][RANDOM_TOPICS] - Error: {e}")
+
+# Run the bot
+bot_client.run_until_disconnected()
